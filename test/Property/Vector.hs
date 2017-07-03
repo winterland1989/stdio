8d54e1133db919b24563f465caaec45c3e576f6d
winter <handong@xiaomi.com>
2017-07-03 15:22:54 +0800

    X

--- test/Property/Vector.hs
+++ test/Property/Vector.hs
@@ -7,6 +7,8 @@ import Test.Tasty.QuickCheck
 import Test.QuickCheck.Property
 import Test.QuickCheck
 import qualified Data.Vector as V
+import Data.Word
+import qualified Data.List as List
 
 propertyVector :: TestTree
 propertyVector = testGroup "vector property" [
@@ -14,5 +16,15 @@ propertyVector = testGroup "vector property" [
             (V.unpack @V.Vector @Int) (V.pack xs)  === xs
     ,   testProperty "unpack @PrimVector @Int . pack == id" . property $ \ xs ->
             (V.unpack @V.PrimVector @Int) (V.pack xs)  === xs
+    ,   testProperty "unpack @PrimVector @Int . pack == id" . property $ \ xs ->
+            (V.unpack @V.PrimVector @Word8) (V.pack xs)  === xs
+
+    ,   testProperty "vector length == list length" . property $ \ xs ->
+            (V.length $ V.pack @V.Vector @Int xs)  ===  List.length xs
+    ,   testProperty "vector length == list length" . property $ \ xs ->
+            (V.length $ V.pack @V.PrimVector @Int xs)  ===  List.length xs
+    ,   testProperty "vector length == list length" . property $ \ xs ->
+            (V.length $ V.pack @V.PrimVector @Word8 xs)  ===  List.length xs
+
 
     ]
